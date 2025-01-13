5db3bb78e34b057ac12c51d9813aade6d9efb82d
Marco van den Oever <marco@flyingpie.nl>
2025-01-13 21:42:05 +0100

    Workspace

--- docker-build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker build -t photinobuilder .
-docker run -it --rm -v $(pwd):/src photinobuilder \
-	make linux-x64 \
-	&& dotnet pack Photino.Native/Photino.Native.csproj --output .
